8ee17202fa5485c8aaeed3ae7ceffd4cf8ca6ee2
zhengxingtian <zhengxingtian@ijiami.cn>
2023-11-24 15:52:57 +0800

    修改公司概况接口数据

--- aktools/adapter/stock.py
+++ aktools/adapter/stock.py
@@ -164,23 +164,23 @@ def custom_stock_overview(symbol: str = "600600") -> pd.DataFrame:
     three_month_ago = current_date - timedelta(days=90)
     if is_a_stock(symbol):
         indicator_temp_df = stock_a_indicator_lg(symbol=symbol)
-        indicator_df = indicator_temp_df[(indicator_temp_df['trade_date'] > three_month_ago.date())]
+        indicator_df = indicator_temp_df.sort_values(by='trade_date', ascending=False).head(1)
     else:
         indicator_temp_df = stock_hk_indicator_eniu(symbol="hk" + symbol)
-        indicator_df = indicator_temp_df[(indicator_temp_df['date'] > three_month_ago.date())]
+        indicator_df = indicator_temp_df.sort_values(by='date', ascending=False).head(1)
     one_year_fund_holder_df = fund_holder_df[(fund_holder_df['截止日期'] > three_month_ago.date())]
     # 排序
     sorted_fund_holder = one_year_fund_holder_df.sort_values(by='持仓数量', ascending=False)
     # 使用head获取前10行数据
-    records_json = {"公司概况": zyjs_df,
+    records_json = {"公司概况": zyjs_df.head(1),
                     "最近一年关键财务指标": b_df,
                     "限售解禁情况": jiejing_df,
-                    "个股指标": indicator_df,
                     "历史分红数据": dividend_df[(dividend_df['实施方案公告日期'] > six_year_ago.date())],
                     "最近1年董监高人员股份变动": hold_change_df,
                     "最近6个月流通股东详情": holder_df[(holder_df['截止日期'] > six_month_ago.date())],
                     "最近3个月持有当前股票的前十大基金": sorted_fund_holder.head(10),
-                    "最近关于此公司的新闻": news_df.head(15)
+                    "最近关于此公司的新闻": news_df.head(10),
+                    "股价概况": indicator_df
                     }
     result_df = pd.Series(records_json).to_frame().T
     result_df.columns = records_json.keys()
