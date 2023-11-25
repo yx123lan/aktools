f8229ba360025d963a618e94fb1982e0434e3f45
tarent <lan4627@gmail.com>
2023-11-26 00:57:17 +0800

    fix import akshare2 bug

--- aktools/adapter/akshare2.py
+++ aktools/adapter/akshare2.py
@@ -1,7 +1,4 @@
 from aktools.adapter.stock import (
     custom_stock_financial_report_sina,
-    custom_stock_overview,
-    custom_stock_overview2,
-    custom_stock_overview3,
-    custom_stock_overview4
+    custom_stock_overview
 )
